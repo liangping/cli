536dde826f92dbfa76a82a41ec853ab7e6e416c9
Tony Arcieri <tony@iqlusion.io>
2019-10-19 14:59:25 -0700

    Initial Abscissa v0.4 boilerplate

---
+++ src/commands/version.rs
@@ -0,0 +1,21 @@
+//! `version` subcommand
+
+#![allow(clippy::never_loop)]
+
+use super::OpenLibraCmd;
+use abscissa_core::{Command, Options, Runnable};
+
+/// `version` subcommand
+#[derive(Command, Debug, Default, Options)]
+pub struct VersionCmd {}
+
+impl Runnable for VersionCmd {
+    /// Print version message
+    fn run(&self) {
+        println!(
+            "{} {}",
+            OpenLibraCmd::name(),
+            OpenLibraCmd::version()
+        );
+    }
+}
