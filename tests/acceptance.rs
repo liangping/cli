42b377a378182c219815bc27d4d90143c512af79
Tony Arcieri <tony@iqlusion.io>
2019-10-21 09:11:45 -0700

    "open-libra config" subcommand
    Adds a subcommand adapted from `libra-swarm` which generates the
    following config files using a random seed:
    
    - consensus_keypair.config.toml
    - network_keypairs.config.toml
    - node.config.toml
    
    Additionally it generates a `peer_info.toml` containing the peer ID,
    `ConsensusPeerInfo` and `NetworkPeerInfo` which correspond to the
    randomly generated private keys, which can be later assembled into the
    `genesis.blob`, `network_peers.config.toml`, `seed_peers.config.toml`,
    etc.
    
    Work to synthesize `genesis.blob` and the global config files will
    happen in a followup PR.

--- tests/acceptance.rs
+++ tests/acceptance.rs
@@ -4,11 +4,14 @@
 //! For more information, see:
 //! <https://docs.rs/abscissa_core/latest/abscissa_core/testing/index.html>
 
-#![deny(warnings, missing_docs, trivial_casts, unused_qualifications)]
 #![forbid(unsafe_code)]
+#![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
 
 use abscissa_core::testing::prelude::*;
 use lazy_static::lazy_static;
+use libra_config::config::{ConsensusConfig, NetworkConfig, NodeConfig, PersistableConfig};
+use open_libra::peer_info::PeerInfo;
+use tempfile::tempdir;
 
 lazy_static! {
     /// Executes your application binary via `cargo run`.
@@ -21,8 +24,22 @@ lazy_static! {
 }
 
 #[test]
-fn start_no_args() {
+fn config_generator() {
     let mut runner = RUNNER.clone();
-    let cmd = runner.arg("config").run();
+
+    let dir = tempdir().unwrap();
+    let cmd = runner
+        .arg("config")
+        .arg("-o")
+        .arg(dir.path())
+        .capture_stdout()
+        .run();
+
     cmd.wait().unwrap().expect_success();
+
+    // Make sure the generated config files load
+    ConsensusConfig::load_config(dir.path().join("consensus_keypair.config.toml"));
+    NetworkConfig::load_config(dir.path().join("network_keypairs.config.toml"));
+    NodeConfig::load_config(dir.path().join("node.config.toml"));
+    PeerInfo::load_config(dir.path().join("peer_info.toml"));
 }
